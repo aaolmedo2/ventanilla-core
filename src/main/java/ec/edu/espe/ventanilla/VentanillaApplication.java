2ad3f4c8be343c6177dd1c6f0c0f047cea534caf
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-11-16 02:48:46 -0500

    Commit inicial del proyecto

---
+++ src/main/java/ec/edu/espe/ventanilla/VentanillaApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.ventanilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentanillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentanillaApplication.class, args);
+	}
+
+}
